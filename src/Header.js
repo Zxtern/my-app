1d9b3d45be00aa59884dbcdb18cf362670e79583
Zxtern <romanjael1@outlook.com>
2025-07-18 22:15:47 -0700

    Added CSS properties to navigation bar and about me page.

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,17 @@ import React from "react";
 
 function Header() {
     return (
-        <header>
-            <img src="/AboutUsPic.jpg" alt="About Us" width="300" height="300"></img>
-            <h1>Little Lemon</h1>
-            <h2>Silicon Valley</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
-            <buton>Reserve Now</buton>
-        </header>
+        <div className="about-container">
+            <header className="about-content">
+                <div className="about-text">
+                    <h1>Little Lemon</h1>
+                    <h2>Silicon Valley</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
+                    <button>Reserve Now</button>
+                </div>
+                <img src="/AboutUsPic.jpg" alt="About Us" width="300" height="300"></img>
+            </header>
+        </div>
     );
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -2,17 +2,19 @@ import React from "react";
 
 function Nav() {
     return (
-        <nav>
-            <img src="/little_lemon_logo.jpg" alt="Little Lemon Logo" width="200" height="80"></img>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#meu">Menu</a></li>
-                <li><a href="#reservations">Reservations</a></li>
-                <li><a href="#order">Order Online</a></li>
-                <li><a href="#login">Login</a></li>
-            </ul>
-        </nav>
+        <div className="nav-container">
+            <nav className="nav-bar">
+                <img src="/little_lemon_logo.jpg" alt="Little Lemon Logo" width="200" height="80"></img>
+                <ul>
+                    <li><a href="home">Home</a></li>
+                    <li><a href="about">About</a></li>
+                    <li><a href="menu">Menu</a></li>
+                    <li><a href="reservations">Reservations</a></li>
+                    <li><a href="order">Order Online</a></li>
+                    <li><a href="login">Login</a></li>
+                </ul>
+            </nav>
+        </div>
     );
 }
 
