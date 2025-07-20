83c412195246243422175abebb829a87e8f3a6ee
Zxtern <romanjael1@outlook.com>
2025-07-20 16:06:27 -0700

    Added Css properties to specials, testimonials, about, and footeer pages. Aligned text and added color. As well as, perfected the Reserve Now button

--- src/Footer.js
+++ src/Footer.js
@@ -2,27 +2,31 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer>
-            <ul>Navigation
-                <li>Home</li>
-                <li>About</li>
-                <li>Menu</li>
-                <li>Reservations</li>
-                <li>Order Online</li>
-                <li>Login</li>
-            </ul>
-            <ul>Contact
-                <li>Address</li>
-                <li>Phone Number</li>
-                <li>Email</li>
-            </ul>
-            <ul>Social Media Links
-                <li>Instagram</li>
-                <li>Facebook</li>
-                <li>TikTok</li>
-            </ul>
-            <p>Copyright © 2025 Little Lemon</p>
+        <div className="footer-container">
+            <footer className="footer-links">
+                <ul>Navigation
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Menu</li>
+                    <li>Reservations</li>
+                    <li>Order Online</li>
+                    <li>Login</li>
+                </ul>
+                <ul>Contact
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+                <ul>Social Media Links
+                    <li>Instagram</li>
+                    <li>Facebook</li>
+                    <li>TikTok</li>
+                </ul>
         </footer>
+            <div className="copyright-container">
+                <p>Copyright © 2025 Little Lemon</p>
+            </div>
+        </div>
     );
 }
 
--- src/Main.js
+++ src/Main.js
@@ -3,25 +3,32 @@ import React from "react";
 function Main() {
     return (
         <main>
+            <div className="specials-container">
+                <h2>Specials</h2>
+                <button>Online Menu</button>
+                <h4>Greek Salad</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
+                <h4>Bruschetta</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
+                <h4>Lemon Dessert</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
+            </div>
             
-            <h2>Specials</h2>
-            <h4>Greek Salad</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
-            <h4>Bruschetta</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
-            <h4>Lemon Dessert</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
+            <div className="testimonials-container">
+                <h2>Testimonials</h2>
+                <h4>User123</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
+                <h4>User456</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
+                <h4>User789</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
+            </div>
             
-            <h2>Testimonials</h2>
-            <h4>User123</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
-            <h4>User456</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
-            <h4>User789</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
-
-
-
+            <div className="details-container">
+                <h2>Little Lemon</h2>
+                <h3>Silicon Valley</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
+            </div>
         </main>
     );
 }
